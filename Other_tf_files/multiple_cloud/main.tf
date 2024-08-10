61614c4eab8a5834ff7af752be97f24ea7c263d7
ambrecodes <113693379+ambrecodes@users.noreply.github.com>
2024-08-10 03:03:45 +0000

    added day 2 updates

---
+++ Other_tf_files/multiple_cloud/main.tf
@@ -0,0 +1,28 @@
+#example - considering that you are using more than one cloud provider
+
+#azure first
+
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+#aws next
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 5.0"
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
