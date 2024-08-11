6ad8c1179b8c2e64b541b8371b5c35433a317123
ambrecodes <113693379+ambrecodes@users.noreply.github.com>
2024-08-11 04:06:56 +0000

    updated conditional statements

---
+++ Terraform/Other_tf_files/conditional_expressions_in_tf.tf
@@ -0,0 +1,40 @@
+#example for conditional expressions in tf 
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "environment" {
+ default = "production"
+}
+
+
+variable "production_CIDR" {
+    description = "CIDR values for production subnet"
+    default = "10.0.0.0/16"
+  
+}
+
+variable "develop_CIDR" {
+  description = "CIDR values for development subnet"
+  default = "10.3.0.0/24"
+}
+
+resource "aws_security_group" "security_group" {
+    name = "example-sg"
+    description = "Example Security Group"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = tcp 
+
+        # CONDITIONAL EXPRESSION : IF THE VALUE OF THE ENVIRONMENT VAR IS PROD, THEN ASSIGN PROD VALUE OR DEVELOP IF ITS NOT
+        cidr_blocks = var.environment == "production" ? [var.production_CIDR] : [var.develop_CIDR]
+
+
+        #SYNTAX IS 
+        # conditiopn ? [true_value] : [false_value]
+    }
+
+
+}
