29a1502deca3887d7e5ad1d53586586540804c99
ambrecodes <113693379+ambrecodes@users.noreply.github.com>
2024-08-12 10:06:35 +0100

    Codespace studious spork jvpq6gq47jj3rq5 (#3)
    * Using modules
    
    * updated deleted files
    
    * Azure documentation

--- Terraform/Other_tf_files/modular_way/variables.tf
+++ Terraform/Other_tf_files/modular_way/variables.tf
@@ -9,13 +9,3 @@ variable "instance_type_value" {
 variable "region_value" {
   description = "Name of the region where the instance will be built"
 }
-
-provider "aws" {
-  region = var.region_value
-}
-
-resource "aws_instance" "this" {
-  ami                     = var.ami_id_value 
-  instance_type           = var.instance_type_value
-} 
-
